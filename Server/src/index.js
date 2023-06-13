0ede51219069fb3e2ce8af2b2d97a9831cafac82
samuelbernal44 <samuelbernal44@gmail.com>
2023-06-13 11:48:25 -0500

    Homework promise integrador: done

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,24 +1,15 @@
-/* eslint-disable linebreak-style */
 const http = require('http');
-const data = require('./utils/data');
+const getCharById = require('./controllers/getCharById');
 
 const PORT = 3001;
+
 http
   .createServer((req, res) => {
-    // eslint-disable-next-line no-console
-    console.log(`Server raised in port ${PORT}`);
     res.setHeader('Access-Control-Allow-Origin', '*');
     const { url } = req;
     if (url.includes('/rickandmorty/character')) {
-      const id = parseInt(url.split('/').pop(), 10);
-      const character = data.find((char) => char.id === id);
-      if (character) {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(character));
-      } else {
-        res.writeHead(404, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ error: 'Character not found' }));
-      }
+      const id = Number(url.split('/').pop());
+      getCharById(res, id);
     }
   })
   .listen(PORT, 'localhost');
