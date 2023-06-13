09a5a3445f6ac5e2c0c33dd590ab2c3149cf5b5d
samuelbernal44 <samuelbernal44@gmail.com>
2023-06-13 11:44:54 -0500

    update a método createRoot de react-dom/client

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,16 +1,17 @@
-import ReactDOM from 'react-dom';
+/* eslint-disable comma-dangle */
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
 
-ReactDOM.render(
+const root = document.getElementById('root');
+
+createRoot(root).render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>,
-  // eslint-disable-next-line comma-dangle
-  document.getElementById('root')
+  </Provider>
 );
