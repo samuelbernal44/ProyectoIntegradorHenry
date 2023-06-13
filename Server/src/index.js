5d7646a058c42601db5116ab3a68369037695ac9
samuelbernal44 <samuelbernal44@gmail.com>
2023-06-12 19:02:33 -0500

    error response

--- Server/src/index.js
+++ Server/src/index.js
@@ -5,17 +5,20 @@ const data = require('./utils/data');
 const PORT = 3001;
 http
   .createServer((req, res) => {
+    // eslint-disable-next-line no-console
     console.log(`Server raised in port ${PORT}`);
     res.setHeader('Access-Control-Allow-Origin', '*');
     const { url } = req;
     if (url.includes('/rickandmorty/character')) {
       const id = parseInt(url.split('/').pop(), 10);
       const character = data.find((char) => char.id === id);
-      res.setHeader('Content-Type', 'application/json');
-      res.end(JSON.stringify(character));
-    } else {
-      res.statusCode = 404;
-      res.end('Not Found');
+      if (character) {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(character));
+      } else {
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ error: 'Character not found' }));
+      }
     }
   })
   .listen(PORT, 'localhost');
