c1b02bb9b3ddb81a6eaacb7a0751453a439bef67
samuelbernal44 <samuelbernal44@gmail.com>
2023-07-14 10:32:49 -0500

    Sequelize Integration part 1: done

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,8 +1,17 @@
+require('dotenv').config();
 const server = require('./app');
+const { conn } = require('./DB_connection');
 
-const PORT = 3001;
+const { PORT } = process.env;
 
-server.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server raised in port ${PORT}`);
-});
+conn
+  .sync({ force: false })
+  .then(() => {
+    server.listen(PORT, () => {
+      // eslint-disable-next-line no-console
+      console.log(`Server raised in port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
---
+++ Server/src/models/User.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    'User',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        isEmail: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    { timestamps: false }
+  );
+};
